b4d3e5b93bd5164859a1f2e948f674db0579e75c
Arnab Ghose <arnab.ghose128@gmail.com>
2024-03-05 10:35:15 +0530

    Update contract.rs

--- reputation-dashboard/contracts/reputation-engine-contract/src/contract.rs
+++ reputation-dashboard/contracts/reputation-engine-contract/src/contract.rs
@@ -41,7 +41,6 @@ pub fn execute(
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
-        // something
         QueryMsg::QueryScoreByDidId { did_id } => to_json_binary(&query_score_by_did_id(deps, did_id)?),
         QueryMsg::QueryScoreByAddress { user_address } => to_json_binary(&query_score_by_address(deps, user_address)?),
     }
